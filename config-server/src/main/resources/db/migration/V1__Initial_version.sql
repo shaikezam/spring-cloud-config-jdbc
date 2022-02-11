9b1ca510ffc1971a7be83c6b664ce268bcc65f2f
Shai Zambrovski <SZambrovski@illusivenetworks.com>
2022-02-11 08:23:35 +0200

    change files

--- config-server/src/main/resources/db/migration/V1__Initial_version.sql
+++ config-server/src/main/resources/db/migration/V1__Initial_version.sql
@@ -17,5 +17,4 @@ create table if not exists properties
 
 INSERT INTO properties (id, application, profile, label, key, value, default_value, date_created, last_updated)
 VALUES
-    ('010ec074-9650-4de4-bca2-e71c86d65e40', 'client', 'dev', 'teva', 'my.value', 'Hello World', 'Hello World', now(), now()),
-    ('9401f74f-7d8f-4923-84d7-fa5ea21e0ada', 'clientf', 'dev', 'teva', 'my.value', 'Hello World', 'Hello World', now(), now());
+    ('010ec074-9650-4de4-bca2-e71c86d65e40', 'client', 'dev', 'teva', 'my.value', 'Hello World', 'Hello World', now(), now());
